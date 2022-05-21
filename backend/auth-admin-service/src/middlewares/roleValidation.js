fd6861bd6108de5dd93cf1a80d95b095a086c54e
Shreyas.Ks.76006642 <Shreyas.Ks@microfocus.com>
2022-05-21 20:00:08 +0530

    Code refactoring

--- backend/auth-admin-service/src/middlewares/roleValidation.js
+++ backend/auth-admin-service/src/middlewares/roleValidation.js
@@ -5,6 +5,7 @@ let { Role, Endpoint, Permission } = require('../sequelize');
 // Globals
 var IN_MEMORY_OBJECT_CACHE_EXPIRATION_AT = Math.floor(new Date().getTime() / 1000) - 100;
 var IN_MEMORY_CACHE = {};
+var IN_MEMORY_ROLE_ENDPOINT_CACHE = {};
 
 async function getEndpointIdMap(){
     console.log(`Building Endpoint ID Map...`);
@@ -23,7 +24,6 @@ async function buildPermissionMap(){
     console.log(`Building Permission Map...`);
     let permissions = await Permission.findAll({});
     await permissions.forEach(p => {
-        console.log(JSON.stringify(p));
         permissionIdMap[p.id] = {
             "name": endpointIdMap[p.endpoint_id].name,
             "endpoint": endpointIdMap[p.endpoint_id].endpoint,
@@ -38,12 +38,13 @@ async function buildPermissionMap(){
 async function buildRoleMap(){
     console.log(`Building Role Map...`);
     let roleMap = {};
+    let roleEndpointMap = {};
     const permissionIdMap = await buildPermissionMap();
     let roles = await Role.findAll({});
     await roles.forEach(r => {
         const permissions = JSON.parse(JSON.stringify(r.permissions)).permissions;
-        console.log(permissions);
         roleMap[r.name] = [];
+        roleEndpointMap[r.name] = [];
         permissions.forEach(p => {
             if(p == '*'){
                 roleMap[r.name].push('*');
@@ -51,9 +52,11 @@ async function buildRoleMap(){
                 return;
             }
             roleMap[r.name].push(permissionIdMap[p]);
+            roleEndpointMap[r.name].push(permissionIdMap[p].name);
             
         });
         IN_MEMORY_CACHE = roleMap;
+        IN_MEMORY_ROLE_ENDPOINT_CACHE = roleEndpointMap;
         IN_MEMORY_OBJECT_CACHE_EXPIRATION_AT = Math.floor(new Date().getTime() / 1000) + 12*60*60;
     }); 
     console.log(`Role Map: ${JSON.stringify(roleMap)}`);
@@ -69,10 +72,9 @@ async function validateRole(req, res, next){
         }
         let endpointName = req.endpointName;      
 
-        let userRoles = req.user.ur;
-        console.log(userRoles);  
+        let userRoles = req.user.ur; 
         for(let i=0; i<userRoles.length; i++){
-            let hasAccessToEndpoints = IN_MEMORY_CACHE[userRoles[i]];
+            let hasAccessToEndpoints = IN_MEMORY_ROLE_ENDPOINT_CACHE[userRoles[i]];
             if (hasAccessToEndpoints.includes('*') || hasAccessToEndpoints.includes(endpointName))
                 return next();  
         }
---
+++ backend/auth-admin-service/src/routes/RoleRoutes/validateRole.route.js
@@ -0,0 +1,102 @@
+/*jshint esversion: 8 */
+/* eslint-disable arrow-parens */
+/* eslint-disable no-console */
+const { Role } = require('../../sequelize');
+const router = require("express").Router();
+
+// Load the Winston logger
+const logger = require('../../winston.conf.js');
+const { validateBodyParamsExistence } = require('../../utils/validateBodyParameters');
+let addEndpointNameToRequest = require('../../middlewares/addEndpointNameToRequest');
+const { validateRole } = require('../../middlewares/roleValidation');
+const authenticateToken = require('../../middlewares/authenticateToken');
+
+/**
+ * @swagger
+ * /updateRole:
+ *   put:
+ *     tags:
+ *       - Role
+ *     name: Update a Role
+ *     summary: Update a Role
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             name:
+ *               description: Unique name given to a role (Ex - User, Moderator, Admin, SuperAdmin)
+ *               required: true
+ *             description:
+ *               type: string
+ *               description: Description about the Role.
+ *             permissions:
+ *               type: object
+ *               description: Permission array (JSON string). Value should be an array of Permission IDs with "permissions" as Key
+ *           required:
+ *             - name
+ *             - description
+ *             - permissions
+ *     responses:
+ *       '201':
+ *         description: Role created successfully.
+ *       '400':
+ *         description: Bad Request ! Permission already exists.
+ *       '401':
+ *         description: Body Parameters validation failed.
+ */
+
+router.post('/validateRole', 
+            addEndpointNameToRequest('update_role'), 
+            authenticateToken,
+            validateRole,
+            async (req, res) => {
+              // Validate weather the request body contains all the parameters or not
+              var bodyParameterValidationResult = validateBodyParamsExistence(req, ['name', 'description', 'permissions']);
+              if (bodyParameterValidationResult.status == false){
+                logger.debug(`Body parameter validation error: ${bodyParameterValidationResult.message}`);
+                return res.status(401).send({
+                  statusCode: 401,
+                  message: bodyParameterValidationResult.message
+                });
+              }
+              try {
+                  // If Role exists, then throw the Error
+                const roleExists = await Role.findOne({ where: { name: req.body.name }});
+                if (roleExists){
+                    logger.debug(`Updating Role ${req.body.name}...`);
+                    logger.debug(`Updated Role details -- Name: ${req.body.name}, Permission: ${JSON.stringify(req.body.permissions)}`);
+                    roleExists.description = req.body.description;
+                    roleExists.permissions = req.body.permissions;
+                    await roleExists.save();
+                    return res.status(201).send({
+                        statusCode: 201,
+                        message: `Role Updated. ID: ${roleExists.id}, Permissions: ${roleExists.permissions}`,
+                      });
+                }
+                else
+                return res.status(400).send({
+                    statusCode: 400,
+                    message: `Bad request !! - Role Doesn't exist. Details: Name - ${req.body.name}`,
+                });
+              } catch (error) {
+                logger.error(err);
+                return res.status(500).send({
+                  statusCode: 500,
+                  message: 'Internal Server Error !!!',
+                  devMessage: err.message,
+                  stackTrace: err.stack
+                });
+              }
+});
+
+
+module.exports=router;
